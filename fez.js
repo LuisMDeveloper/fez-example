fe9aa61732e211fee53b106448c5dc0c2e30e227
Luis Manuel Ramirez Vargas <luis.manuel.ramirez.91@gmail.com>
2014-03-01 17:45:31 -0600

    Fez init

---
+++ fez.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Luis Manuel Ramirez Vargas <luis.manuel.ramirez.91@gmail.com> on 3/1/14.
+ */
+
+var fez = require("fez");
+
+exports.build = function(spec) {
+};
+
+exports.default = exports.build;
+
+fez(module);
